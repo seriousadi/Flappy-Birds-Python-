fc46ef0984d3d45584f0993830f632f584208e0c
Aditya Kumar <aaz22hsns@gmail.com>
2023-06-22 14:47:00 +0530

    Only collision is left to add

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 import os
 from Imagegenerator import ImageHandler
+from random import randint
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((400, 550))
 clock = pygame.time.Clock()
 
+
+# give random int
+def getRandInt():
+    return -(randint(0, 260))
+
+
 # initializing Imagehandler
 Imagehandler = ImageHandler(screen)
-background_and_pipe_speed = 0
+background_speed = 0
 bird_y = Imagehandler.y
-
+pipe1x, pipe1y = 400, getRandInt()
+pipe2x, pipe2y = 600, getRandInt()
+pipe3x, pipe3y = 800, getRandInt()
 running = True
 current_directory = os.path.dirname(__file__)
 while running:
@@ -25,8 +34,12 @@ while running:
     screen.fill("#71C5CF")
 
     # moving background
-    Imagehandler.render_background(background_and_pipe_speed)
-    background_and_pipe_speed -= 1
+    Imagehandler.render_background(background_speed)
+
+    if background_speed < -504:
+        background_speed = 0
+        y_for_pipes = getRandInt()
+    background_speed -= 5
     # movements of bird
     # when mouse isn't clicked the bird falls and when mouse is clicked it fly
     bird_y += 15
@@ -38,9 +51,21 @@ while running:
     else:
         bird_y -= 50
         Imagehandler.render_birdup(bird_y)
+    # managing pipes movements
+    pipe1x -= 5
+    pipe2x -= 5
+    pipe3x -= 5
+    if pipe1x <= -66:
+        pipe1x = pipe3x + 200
+        pipe1y = getRandInt()
+    if pipe2x <= -66:
+        pipe2x = pipe1x + 200
+        pipe2y = getRandInt()
+    if pipe3x <= -66:
+        pipe3x = pipe2x + 200
+        pipe3y = getRandInt()
 
-    Imagehandler.render_pipes(0, -260)
-
+    Imagehandler.render_pipes(x1=pipe1x, y1=pipe1y, x2=pipe2x, y2=pipe2y, x3=pipe3x, y3=pipe3y)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
